2a3f66be7d82dc11ba3120f64225b96320ae95d9
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-10-12 19:38:47 +0100

    Update project config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <n-header> </n-header>
+      <n-banner> </n-banner>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/home/Header.vue'
+import Banner from './components/home/Banner.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'n-header': Header,
+    'n-banner': Banner
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="sass">
+main
+  position: relative
+  display: table
+  width: 100%
+  background-image: url('https://images.unsplash.com/photo-1554306274-f23873d9a26c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80') //no-repeat center center fixed
+  background-size: cover
+  height: 100vh
 </style>
